dee51e9ce77de3534b41e2e80cd40ef485b21d14
Ajay Balkaran <ajaybalkaran@gmail.com>
2020-01-27 16:50:46 -0500

    Create app.js
    created a main javascript file and the functions need to open and close the nav - Mobile view

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.querySelector(".nav-links").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.querySelector(".nav-links").style.height = "0%";
+  }
